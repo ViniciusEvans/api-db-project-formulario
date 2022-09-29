281811e5aa20596231e6dbab54125d4724d3ec03
Vinicius <vinibrother.evansz2@gmail.com>
2022-09-29 17:22:28 -0300

    feat: add global error handler middleware

---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorHandler } from "../helpers/errorHandler";
+
+export const globalErrorHandler = async (
+  error: Error & Partial<ErrorHandler>,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const message: string = error.statusCode
+    ? error.message
+    : "Internal Server Error";
+  const statusCode: number = error.statusCode ?? 500;
+  return res.status(statusCode).json({ message });
+};
